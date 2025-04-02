5a3eb4c4877dc661bde4327fc78d0ef90d644df9
Jonathan LAURENT <johjo@ytreza.org>
2025-04-02 07:25:19 +0200

    wip

--- tests/test_minesweeper_04_02.py
+++ tests/test_minesweeper_04_02.py
@@ -5,3 +5,4 @@
 # for the mine field : .* the solved mine field is 1*
 # for the mine field : *. the solved mine field is *1
 # for the mine field : ... the solved mine field is 000
+# for the mine field : ..* the solved mine field is 01*
