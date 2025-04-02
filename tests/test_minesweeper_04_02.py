eeacc5a8f2f213ac61761b757ea3071ad09dcb2b
Jonathan LAURENT <johjo@ytreza.org>
2025-04-02 07:36:54 +0200

    green

--- tests/test_minesweeper_04_02.py
+++ tests/test_minesweeper_04_02.py
@@ -15,18 +15,15 @@ def minesweeper(field: str) -> str:
     solved_field = ""
     if field == "*":
         solved_field += "*"
-        return solved_field
     elif field == "**":
         solved_field += "*"
         solved_field += "*"
-        return solved_field
     elif field == "..":
         solved_field += "0"
         solved_field += "0"
-        return solved_field
     else:
         solved_field += "0"
-        return solved_field
+    return solved_field
 
 
 def test_minesweeper_01():
