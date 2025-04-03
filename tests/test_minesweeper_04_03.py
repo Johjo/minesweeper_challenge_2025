8c90a525616ce8316abedeacc4f47563264b938b
Jonathan <johjo@ytreza.org>
2025-04-03 06:07:40 +0200

    green

---
+++ tests/test_minesweeper_04_03.py
@@ -0,0 +1,12 @@
+# for the mine field : . the solved mine field is 0
+# for the mine field : * the solved mine field is *
+# for the mine field : .. the solved mine field is 00
+# for the mine field : ** the solved mine field is **
+# for the mine field : .* the solved mine field is 1*
+# for the mine field : *. the solved mine field is *1
+# for the mine field : ... the solved mine field is 000
+# for the mine field : ..* the solved mine field is 01*
+# for the mine field : *.. the solved mine field is *10
+# for the mine field : *.* the solved mine field is *2*
+# for the mine field : .*. the solved mine field is 1*1
+
